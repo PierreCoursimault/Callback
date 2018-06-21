98e115abcecad6ed307da65ab58d001d692e6193
qurichard1 <quentin-le-gaulois@hotmail.fr>
2018-06-21 20:34:20 +0200

    Debut de Setup Menu
    Avec des placeholder(moche oui)
    On peut passer d'une ligne a l'autre en valider avec A et revenir avec B

---
+++ Assets/Scripts/TextMenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextMenuScript : MonoBehaviour {
+
+	public Sprite unlight;
+	public Sprite highlight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
